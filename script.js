e8626d360ff89b1ba72112b78be5ef4c2c38556f
mdakbarhossain1 <mdakbarhossain16@gmail.com>
2024-05-25 19:49:53 +0600

    slideer

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ burger.addEventListener("click", () => {
 });
 
 // console.log("first");
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  // spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
